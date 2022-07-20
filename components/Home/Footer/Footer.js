01ed583674f7c5f50a70ce020f0aca8bc75fa654
Shajibul Alam Shihab <82690571+shihab-2021@users.noreply.github.com>
2022-07-20 22:17:01 +0600

    react scroll added

--- components/Home/Footer/Footer.js
+++ components/Home/Footer/Footer.js
@@ -5,10 +5,28 @@ import { BsLinkedin } from "react-icons/bs";
 import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import styles from "../../../styles/Footer.module.css";
+import ScrollToTop from "react-scroll-to-top";
 
 const Footer = () => {
   return (
     <div className="pt-5 mt-5">
+      <ScrollToTop
+        smooth
+        top="500"
+        color="#ffffff"
+        viewBox="0 0 330 330"
+        svgPath="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M255.606,205.606
+        C252.678,208.535,248.839,210,245,210s-7.678-1.464-10.606-4.394l-69.396-69.393l-69.392,69.393c-5.857,5.858-15.355,5.858-21.213,0
+        c-5.858-5.857-5.858-15.355,0-21.213l79.998-80c2.813-2.813,6.628-4.394,10.606-4.394c3.979,0,7.793,1.58,10.607,4.394l80.002,80
+        C261.465,190.251,261.465,199.749,255.606,205.606z"
+        style={{
+          borderRadius: "20px",
+          // paddingLeft: "6px",
+          backgroundColor: "#2bfde9",
+          zIndex: "3",
+          // backgroundColor: "#4f46e5",
+        }}
+      />
       <footer className={styles.footer}>
         <div className={`${styles.footer_blur1} blur`}></div>
         <div className={`${styles.footer_blur2} blur`}></div>
